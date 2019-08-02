50c3de98b46330f312114bef9eec897596dd0884
Chihiro <zgChihiro@gmail.com>
2019-08-02 17:14:14 +0800

    完成.4

--- 文件下载+进度条/新版下载/delolReg.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
-cd /d "%~dp0"
-reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Âò¼Ñ¾Ó¿Í»§¶Ë1.0  /f
